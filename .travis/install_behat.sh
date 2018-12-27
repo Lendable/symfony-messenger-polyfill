3034d641ab456f16ee657749d4444f1f5d8e864c
kunicmarko20 <kunicmarko20@gmail.com>
2018-12-27 15:10:39 +0000

    add composer scripts

--- .travis/install_behat.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -ev
-
-composer update --dev --prefer-lowest
--- .travis/script_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -ev
-
-vendor/bin/phpunit -c phpunit.xml.dist --coverage-clover build/logs/clover.xml
